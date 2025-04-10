1c6ccabc12c6278beaa0b375d567937505552f32
Manoj-HT <manulogan101@gmail.com>
2025-04-10 23:35:49 +0530

    # 1.0.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'hello-kannada' title`, () => {
+  it(`should have the 'kannada-teacher' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('hello-kannada');
+    expect(app.title).toEqual('kannada-teacher');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, hello-kannada');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, kannada-teacher');
   });
 });
---
+++ src/app/topic/topic.component.ts
@@ -0,0 +1,33 @@
+import { Component, inject, input, signal } from '@angular/core';
+import { KeyboardComponent } from '../keyboard/keyboard.component';
+import { KeyboardService } from '../services/keyboard/keyboard.service';
+import { AudioService } from '../services/audio/audio.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'topic',
+  imports: [KeyboardComponent],
+  templateUrl: './topic.component.html',
+  styleUrl: './topic.component.scss'
+})
+export class TopicComponent {
+  topicId = input();
+  chapterId = input<number>(0);
+  keyBoardService = inject(KeyboardService)
+  audioService = inject(AudioService)
+  route = inject(Router)
+  ngOnInit(){
+    // this.audioService.speakKannada("ನಮಸ್ಕಾರ, ನೀವು ಹೇಗಿದ್ದೀರಿ?")
+    // this.audioService.speakKannada("Hi How are you")
+  }
+
+
+  addWord(value: string){
+   console.log(value);
+  }
+
+  goBack(){
+    this.route.navigate(["chapter", this.chapterId()])
+  }
+}
+
