9077c89213e7c5b084f9bd898631bfb2809ac7c1
Manoj-HT <manulogan101@gmail.com>
2025-04-28 07:26:40 +0530

    # 1.0.7

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { isPlatformBrowser } from '@angular/common';
 import CONSTANTS from '../global/variables/constants';
 import { ApiService } from '../services/api/api.service';
+import { AppUpdateService } from '../services/app-update/app-update.service';
 
 @Component({
   selector: 'book',
@@ -19,6 +20,7 @@ export class BookComponent {
   route = inject(Router)
   platformId = inject(PLATFORM_ID)
   api = inject(ApiService);
+  updateService = inject(AppUpdateService)
   isAppUpdated = signal<{
     isReleased: boolean;
     isUpdated: boolean;
@@ -31,123 +33,8 @@ export class BookComponent {
 
   ngOnInit() {
     if (!isPlatformBrowser(this.platformId)) return;
-    if (this.checkNextDay()) {
-      this.loadAppConfig();
-    }
+    this.updateService.init();
     this.loadUserData();
-    this.checkAppUpdated();
-  }
-
-  checkNextDay(): boolean {
-    const lastUpdatedOnString = localStorage.getItem(CONSTANTS.localStorageLastUpdatedOn)
-    if (!lastUpdatedOnString) return true;
-    const lastUpdatedOn = new Date(Number(lastUpdatedOnString))
-    return lastUpdatedOn.toDateString() !== new Date().toDateString();
-  }
-
-  loadAppConfig() {
-    this.api.getAppConfig().subscribe({
-      next: (res) => {
-        const appConfigString = localStorage.getItem(
-          CONSTANTS.localStorageAppCofigKey
-        );
-        if (!appConfigString) {
-          this.initApp(res);
-        } else {
-          this.updateApp(res);
-        }
-      },
-    });
-  }
-
-  initApp(appConfig: AppConfig) {
-    localStorage.setItem(
-      CONSTANTS.localStorageAppCofigKey,
-      JSON.stringify(appConfig)
-    );
-    localStorage.setItem(CONSTANTS.localStorageLastUpdatedOn, String(Date.now()))
-  }
-
-  updateApp(res: AppConfig) {
-    const appConfigString = localStorage.getItem(
-      CONSTANTS.localStorageAppCofigKey
-    );
-    const appConfig = JSON.parse(appConfigString as string) as AppConfig;
-    const isNewApp =
-      this.compareVersions(res.appVersion, appConfig.appVersion);
-    const isNewContent =
-      this.compareVersions(res.contentVersion, appConfig.contentVersion) === 1;
-    if (isNewApp === 1) {
-      this.isAppUpdated.update((res) => {
-        return {
-          isReleased: true,
-          isUpdated: false,
-          changeLog: []
-        }
-      })
-    } else if (isNewApp == 0) {
-      localStorage.setItem(CONSTANTS.localStorageAppCofigKey, JSON.stringify(res))
-    }
-    // TODO: read and display notification for content update
-  }
-
-  checkAppUpdated() {
-    const appUpdated = localStorage.getItem(CONSTANTS.localStorageAppUpdated)
-    if (appUpdated) {
-      this.api.getChangeLog().subscribe({
-        next: (res) => {
-          this.isAppUpdated.update(() => ({
-            isReleased: false,
-            isUpdated: appUpdated === 'true',
-            changeLog: this.parseChangelog(res)
-          }))
-          localStorage.setItem(CONSTANTS.localStorageLastUpdatedOn, String(Date.now()))
-        },
-      });
-    }
-  }
-
-  closeChangeLog() {
-    localStorage.removeItem(CONSTANTS.localStorageAppUpdated)
-  }
-
-  reloadApp() {
-    localStorage.setItem(CONSTANTS.localStorageAppUpdated, String(true))
-    location.reload()
-  }
-
-  parseChangelog(text: string): ChangeLog[] {
-    const entries: ChangeLog[] = [];
-    const parts = text.split(/^#\s+/gm).filter(Boolean);
-    for (const part of parts) {
-      const lines = part.trim().split('\n');
-      const versionLine = lines[0].trim();
-      const changes = lines
-        .slice(1)
-        .map((line) => line.trim())
-        .filter((line) => line.length > 0);
-
-      entries.push({
-        version: versionLine,
-        changes,
-      });
-    }
-
-    return entries;
-  }
-
-  compareVersions(a: string, b: string) {
-    const aParts = a.split('.').map(Number);
-    const bParts = b.split('.').map(Number);
-    const length = Math.max(aParts.length, bParts.length);
-    for (let i = 0; i < length; i++) {
-      const aNum = aParts[i] || 0;
-      const bNum = bParts[i] || 0;
-
-      if (aNum > bNum) return 1;
-      if (aNum < bNum) return -1;
-    }
-    return 0;
   }
 
   loadUserData() {
--- src/app/services/app-update/app-update.service.ts
+++ src/app/services/app-update/app-update.service.ts
@@ -1,9 +1,140 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import CONSTANTS from '../../global/variables/constants';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppUpdateService {
 
-  constructor() { }
+  private api = inject(ApiService);
+  isAppUpdated = signal<{
+    isReleased: boolean;
+    isUpdated: boolean;
+    changeLog: ChangeLog[]
+  }>({
+    isReleased: false,
+    isUpdated: false,
+    changeLog: []
+  })
+
+  init() {
+    if (this.checkNextDay()) {
+      this.loadAppConfig();
+    }
+    this.checkAppUpdated();
+  }
+
+  checkAppUpdated() {
+    const appUpdated = localStorage.getItem(CONSTANTS.localStorageAppUpdated)
+    if (appUpdated) {
+      this.api.getChangeLog().subscribe({
+        next: (res) => {
+          this.isAppUpdated.update(() => ({
+            isReleased: false,
+            isUpdated: appUpdated === 'true',
+            changeLog: this.parseChangelog(res)
+          }))
+          localStorage.setItem(CONSTANTS.localStorageLastUpdatedOn, String(Date.now()))
+        },
+      });
+    }
+  }
+
+  reloadApp() {
+    localStorage.setItem(CONSTANTS.localStorageAppUpdated, String(true))
+    location.reload()
+  }
+
+  closeChangeLog() {
+    localStorage.removeItem(CONSTANTS.localStorageAppUpdated)
+  }
+
+  parseChangelog(text: string): ChangeLog[] {
+    const entries: ChangeLog[] = [];
+    const parts = text.split(/^#\s+/gm).filter(Boolean);
+    for (const part of parts) {
+      const lines = part.trim().split('\n');
+      const versionLine = lines[0].trim();
+      const changes = lines
+        .slice(1)
+        .map((line) => line.trim())
+        .filter((line) => line.length > 0);
+
+      entries.push({
+        version: versionLine,
+        changes,
+      });
+    }
+
+    return entries;
+  }
+
+  checkNextDay(): boolean {
+    const lastUpdatedOnString = localStorage.getItem(CONSTANTS.localStorageLastUpdatedOn)
+    if (!lastUpdatedOnString) return true;
+    const lastUpdatedOn = new Date(Number(lastUpdatedOnString))
+    return lastUpdatedOn.toDateString() !== new Date().toDateString();
+  }
+
+  loadAppConfig(): void {
+    this.api.getAppConfig().subscribe({
+      next: (res) => {
+        const appConfigString = localStorage.getItem(
+          CONSTANTS.localStorageAppCofigKey
+        );
+        if (!appConfigString) {
+          this.initApp(res);
+        } else {
+          this.updateApp(res);
+        }
+      },
+    });
+  }
+
+  private initApp(appConfig: AppConfig) {
+    localStorage.setItem(
+      CONSTANTS.localStorageAppCofigKey,
+      JSON.stringify(appConfig)
+    );
+    localStorage.setItem(CONSTANTS.localStorageLastUpdatedOn, String(Date.now()))
+  }
+
+  private updateApp(res: AppConfig) {
+    const appConfigString = localStorage.getItem(
+      CONSTANTS.localStorageAppCofigKey
+    );
+    const appConfig = JSON.parse(appConfigString as string) as AppConfig;
+    const isNewApp =
+      this.compareVersions(res.appVersion, appConfig.appVersion);
+    const isNewContent =
+      this.compareVersions(res.contentVersion, appConfig.contentVersion) === 1;
+    if (isNewApp === 1) {
+      this.isAppUpdated.update((res) => {
+        return {
+          isReleased: true,
+          isUpdated: false,
+          changeLog: []
+        }
+      })
+    } else if (isNewApp == 0) {
+      localStorage.setItem(CONSTANTS.localStorageAppCofigKey, JSON.stringify(res))
+    }
+    // TODO: read and display notification for content update
+  }
+
+
+  compareVersions(a: string, b: string) {
+    const aParts = a.split('.').map(Number);
+    const bParts = b.split('.').map(Number);
+    const length = Math.max(aParts.length, bParts.length);
+    for (let i = 0; i < length; i++) {
+      const aNum = aParts[i] || 0;
+      const bNum = bParts[i] || 0;
+
+      if (aNum > bNum) return 1;
+      if (aNum < bNum) return -1;
+    }
+    return 0;
+  }
 }
